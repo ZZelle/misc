1d9db55215a50acc86d1eb30b6f8b025bb5bc4a0
zzelle <zzelle@none.com>
2015-11-14 11:30:49 +0100

    Support ipython3 and recent ipython versions

--- ipython/profile_default/startup/99-print-imported-modules.py
+++ ipython/profile_default/startup/99-print-imported-modules.py
@@ -1,3 +1,4 @@
 from inspect import ismodule
+
 importeds = [x for x, y in globals().items() if ismodule(y) and not x[0] == '_']
-print 'Imported modules:', ' '.join(sorted(importeds))
+print('Imported modules: %s' % ' '.join(sorted(importeds)))
