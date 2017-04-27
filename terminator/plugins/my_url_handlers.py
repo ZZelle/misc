f3f0ea261e19f0d51e651494ccbeb278da589c2c
zzelle <zzelle@none.com>
2017-04-27 22:50:46 +0200

    Update terminator URL plugin

--- terminator/plugins/my_url_handlers.py
+++ terminator/plugins/my_url_handlers.py
@@ -1,15 +1,14 @@
-import re
-from  terminatorlib import plugin
+from terminatorlib import plugin
 
 
-AVAILABLE = ['OpenStackURLHandler']
-
-
-class OpenStackURLHandler(plugin.URLHandler):
-    handler_name = 'os-launchpad-url'
-    match = r'\b(Closes|Partial|Related)-Bug:?\s?#?\d+\b'
+class JiraURLHandler(plugin.URLHandler):
+    handler_name = 'pegjira-url'
+    nameopen = "Open PegJira"
+    namecopy = "Copy PegJira link"
+    match = r'\<COMC-[0-9]+'
 
     def callback(self, url):
-        print '#' * 150
-        item = re.search('r[^\d](\d+)$', url).group(0)
-        return 'https://bugs.launchpad.net/bugs/%s' % item
+        return 'http://pegjira.pegasus.theresis.org/browse/%s' % url
+
+
+AVAILABLE = [JiraURLHandler.__name__]
